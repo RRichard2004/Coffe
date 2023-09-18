b614d67b9f620a19e61b7d44f0276e9b76b983b0
Main_PC <ricsiruszo90@gmail.com>
2023-09-18 23:16:49 +0200

    1. clean xamarin, colors

---
+++ Coffe/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Coffe.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Coffe
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Coffe/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Coffe.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Coffe
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
